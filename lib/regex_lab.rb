30e20d73f33dbc1075455690398f0eda0627e9b1
maimuna omar <maymunaabdi07@gmail.com>
2023-07-04 23:15:19 +0300

    done

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,20 @@
 def starts_with_a_vowel?(word)
-
-end
-
-def words_starting_with_un_and_ending_with_ing(text)
-
-end
-
-def words_five_letters_long(text)
-
-end
-
-def first_word_capitalized_and_ends_with_punctuation?(text)
-
-end
-
-def valid_phone_number?(phone)
-
-end
+    word.match?(/\A[aeiou]/i)
+  end
+  
+  def words_starting_with_un_and_ending_with_ing(text)
+    text.scan(/\bun\w*ing\b/i)
+  end
+  
+  def words_five_letters_long(text)
+    text.scan(/\b\w{5}\b/)
+  end
+  
+  def first_word_capitalized_and_ends_with_punctuation?(text)
+    text.match?(/\A[A-Z].*[[:punct:]]\z/)
+  end
+  
+  def valid_phone_number?(phone)
+    phone.match?(/^\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}$/)
+  end
+  
